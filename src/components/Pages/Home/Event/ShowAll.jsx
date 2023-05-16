d20c54ea9c90b11ad30a765dea9488844c5dbf3a
ITP <RayhanUddinFarhad11@gmail.com>
2023-05-16 21:41:54 +0600

    search completed

--- src/components/Pages/Home/Event/ShowAll.jsx
+++ src/components/Pages/Home/Event/ShowAll.jsx
@@ -1,8 +1,10 @@
 import React, { useEffect, useState } from 'react';
-import { useLoaderData } from 'react-router-dom';
+import { Form, useLoaderData } from 'react-router-dom';
 import Events from './Events';
+import { useForm } from 'react-hook-form';
 
 const ShowAll = () => {
+    const { register, handleSubmit, watch, formState: { errors } } = useForm();
 
 
     const [data , setData ] = useState ([])
@@ -31,8 +33,22 @@ const ShowAll = () => {
 
      }, [])
 
+     
 
 
+const handleSearchSubmit = (e) => {
+e.preventDefault()
+
+const search = e.target.search.value;
+
+console.log (search)
+
+fetch (`http://localhost:7000/search/${search}`)
+.then (res => res.json())
+.then (data => setData (data))
+
+
+}
 
 
 
@@ -42,6 +58,26 @@ const ShowAll = () => {
     return (
 
         <>
+
+<div className="flex justify-center">
+            <div className="form-control w-full sm:w-auto">
+              <Form onSubmit={handleSearchSubmit} className="input-group">
+                <input
+                  type="text"
+                  placeholder="Search…"
+                  className="input input-bordered"
+                  name='search'
+                />
+                <button className="btn btn-primary">
+
+                  Search
+                </button>
+              </Form>
+            </div>
+
+
+          </div>
+
 
         <button onClick={handlesortDate} className='btn'>sort by date</button>
         <div className='grid lg:grid-cols-4 gap-5'>
